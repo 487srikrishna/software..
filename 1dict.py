d674312c1e9456297d0d90b1789608fe2b400d02
487srikrishna <skpcs2005@gmail.com>
2026-06-05 10:13:11 +0530

    Add files via upload

---
+++ 1dict.py
@@ -0,0 +1,11 @@
+dict ={}
+print("empty dictonary:")
+print(dict)
+
+dict=[{1:'mon',2:'tue',3:'wed'}]
+print("dictionary with the use of dict:")
+print(dict)
+
+dict=([(1,'jan'),(2,'fed')])
+print("dictionary with each as apair:")
+print(dict)
